15d87bbc24505ecb38f257f2942ae61aa9e06271
Tiago Silva Cardoso - 06453943514 <tiago.s.cardoso@animaeducacao.com.br>
2023-10-05 11:50:25 -0300

    criando layout da view de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CoolIcon from '@/assets/icons/CoolIcon.vue';
+</script>
 
 <template>
-  <h1>Login View</h1>
+  <div
+    class="flex flex-col md:flex-row min-h-screen lg:max-h-screen overflow-hidden"
+  >
+    <div class="flex-1 flex-wrap content-center hidden lg:flex">
+      <CoolIcon class="max-w-full" />
+    </div>
+    <div class="flex-1 flex flex-col justify-center px-20">
+      <h1 class="text-6xl font-bold mb-6 text-black">
+        <span class="text-amber">L</span>ogin
+      </h1>
+      <p class="text-xl font-light">Acesse a sua conta no Abra.</p>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      textColor: {
+        amber: '#e7b646',
+        black: 'rgb(50 50 50)'
+      }
+    }
   },
   plugins: []
 };
